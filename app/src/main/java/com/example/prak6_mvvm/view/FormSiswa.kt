146d8e5dd02dca5cf6bd95ed6715096c3b27fb1a
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 09:42:01 +0700

    feat: add Scaffold and configure the TopAppBar

--- app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
@@ -1,5 +1,10 @@
 package com.example.prak6_mvvm.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -7,7 +12,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.prak6_mvvm.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormSiswa(
     pilihanJK: List<String>,
@@ -19,5 +29,9 @@ fun FormSiswa(
     var txtGender by remember {mutableStateOf("")}
     val listData: MutableList<String> = mutableListOf(txtNama,txtAlamat, txtGender)
 
+    Scaffold(modifier = Modifier,
+        topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name), color = Color.White,
+            colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.purple_500))) }) }
+    ) {  }
 
 }
