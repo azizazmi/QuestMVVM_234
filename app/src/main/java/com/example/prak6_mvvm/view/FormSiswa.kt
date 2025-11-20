23909ddfe4276a41e581cd48c56ee245bd6ef191
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 15:27:12 +0700

    feat: add button to submit after filling the form

--- app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -116,6 +118,13 @@ fun FormSiswa(
                 }
             )
             Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(1f),
+                enabled = txtAlamat.isNotEmpty(),
+                onClick = {onSubmitButtonClicked(listData)}
+            ) {
+                Text(text = stringResource(R.string.submit))
+            }
         }
 
     }
