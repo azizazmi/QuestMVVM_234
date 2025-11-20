ae76101ad2498e5012fc6b8f8a4039f088c25f9d
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 15:57:05 +0700

    feat: add composable function TampilSiswa and its parameters

--- app/src/main/java/com/example/prak6_mvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/TampilSiswa.kt
@@ -1,2 +1,11 @@
 package com.example.prak6_mvvm.view
 
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+
+@Composable
+fun TampilSiswa (
+    statusUiSiswa: Siswa,
+    onBackButtonClicked: () -> Unit
+) {}
