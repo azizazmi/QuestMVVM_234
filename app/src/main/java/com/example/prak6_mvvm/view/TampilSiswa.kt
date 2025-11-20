9b830b5b841e2a24febab808c6e88557311d1533
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 16:29:54 +0700

    feat: inisialisasi Scaffold; mengatur topbar dan inisialisasi body isiRuang

--- app/src/main/java/com/example/prak6_mvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/TampilSiswa.kt
@@ -1,12 +1,20 @@
 package com.example.prak6_mvvm.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.prak6_mvvm.R
 import com.example.prak6_mvvm.model.Siswa
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilSiswa (
     statusUiSiswa: Siswa,
@@ -18,4 +26,20 @@ fun TampilSiswa (
         Pair(stringResource(R.string.alamat), statusUiSiswa.alamat)
     )
 
+    Scaffold(
+        modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        stringResource(R.string.detail),
+                        color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(R.color.purple_500))
+            )
+        }
+    ) { isiRuang ->
+
+
+    }
 }
