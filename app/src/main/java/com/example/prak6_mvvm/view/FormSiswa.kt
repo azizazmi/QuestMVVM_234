c032738eed5c8c5d1e83247a23f50b2fd793f8d4
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 14:56:54 +0700

    feat: inisialisasi body Scaffold bernama isiRuang dan add Column layout beserta modifiernya

--- app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
@@ -1,5 +1,8 @@
 package com.example.prak6_mvvm.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -33,6 +37,14 @@ fun FormSiswa(
         topBar = { TopAppBar(title = {
             Text(stringResource(R.string.app_name), color = Color.White)},
             colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.purple_500))) }
-    ) {  }
+    ) { isiRuang ->
+
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) { }
+
+    }
 
 }
