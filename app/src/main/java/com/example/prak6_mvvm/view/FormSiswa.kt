4a5dd84c1a26f083f916612c57e71d94ecac842d
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 09:30:49 +0700

    feat: add function FormSiswa with modifier only (still empty)

--- app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
@@ -1,2 +1,11 @@
 package com.example.prak6_mvvm.view
 
+import androidx.compose.ui.Modifier
+
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
