c2810243fc366b2b687c665d157bd9c622828382
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 16:06:19 +0700

    feat: inisialisasi variable berbentuk list

--- app/src/main/java/com/example/prak6_mvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/TampilSiswa.kt
@@ -3,9 +3,19 @@ package com.example.prak6_mvvm.view
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.prak6_mvvm.R
+import com.example.prak6_mvvm.model.Siswa
 
 @Composable
 fun TampilSiswa (
     statusUiSiswa: Siswa,
     onBackButtonClicked: () -> Unit
-) {}
+) {
+    val items = listOf(
+        Pair(stringResource(R.string.nama), statusUiSiswa.nama),
+        Pair(stringResource(R.string.gender), statusUiSiswa.gender),
+        Pair(stringResource(R.string.alamat), statusUiSiswa.alamat)
+    )
+
+}
