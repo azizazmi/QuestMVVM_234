a087f6b10884f94fbf0443ee182c294131985f49
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 09:34:06 +0700

    feat: add variables in FormSiswa function

--- app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/prak6_mvvm/view/FormSiswa.kt
@@ -1,11 +1,23 @@
 package com.example.prak6_mvvm.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+@Composable
 fun FormSiswa(
     pilihanJK: List<String>,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
 ){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember {mutableStateOf("")}
+    var txtGender by remember {mutableStateOf("")}
+    val listData: MutableList<String> = mutableListOf(txtNama,txtAlamat, txtGender)
+
 
 }
