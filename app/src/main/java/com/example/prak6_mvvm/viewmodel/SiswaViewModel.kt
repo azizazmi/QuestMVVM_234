bbfe234296f31131acf075fe5f7083d1cddc5643
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-20 09:20:24 +0700

    feat: add public variable statusUI as read-only state flow

--- app/src/main/java/com/example/prak6_mvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/prak6_mvvm/viewmodel/SiswaViewModel.kt
@@ -3,7 +3,12 @@ package com.example.prak6_mvvm.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.prak6_mvvm.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+
 }
